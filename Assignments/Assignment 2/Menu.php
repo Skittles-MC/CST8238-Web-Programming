69d025b955bb83715c7973363638f8cb6bc5993a
Skittles-MC <54918149+Skittles-MC@users.noreply.github.com>
2019-09-09 16:12:11 -0400

    Add files via upload

---
+++ Assignments/Assignment 2/Menu.php
@@ -0,0 +1,30 @@
+   
+      <div id="top-menu">
+         <a href="http://choy0007.com/CST8238/Labs/Lab1/Lab1.htm"style=" font-size: 23px"; >Lab 1</a>
+         <a href="http://choy0007.com/CST8238/Labs/Lab2/"style=" font-size: 23px";">Lab 2</a>
+         <a href="http://choy0007.com/CST8238/Labs/Lab3/"style=" font-size: 23px";">Lab 3</a>
+         <a href="http://choy0007.com/CST8238/Labs/Lab4/"style=" font-size: 23px";">Lab 4</a>
+         <a href="http://choy0007.com/CST8238/Labs/Lab5/"style=" font-size: 23px";">Lab 5</a>
+         <a href="http://choy0007.com/CST8238/Labs/Lab6/"style=" font-size: 23px";">Lab 6</a>
+         <a href="http://choy0007.com/CST8238/Labs/Lab7/"style=" font-size: 23px";">Lab 7</a>
+      </div>
+      
+      
+      
+       <div id="side-menu">
+         <table  class="side-menu">
+            
+            <tr>
+               <td><a href="http://choy0007.com/CST8238/Labs/Assignments/Assignment2/index.php">index.php</a></td>
+            </tr>
+            
+            <tr>
+               <td><a href="http://choy0007.com/CST8238/Labs/Assignments/Assignment2/Assign2/travel.php">travel.php</a></td>
+            </tr>
+             
+            
+     
+            
+
+         </table>
+      </div>
---
+++ Assignments/Assignment 2/index.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<?php
+include("Header.php");
+include("Menu.php");
+?>
+<html >
+     
+     
+      <div id="content">
+         <div>
+            <h1>Welcome! ^-^ </h1>
+            <p>Hello and welcome again!. Use the left to navigate to Assignment 2. But there is currently no way back to this index!
+            </p>
+            <br/>
+            <hr>
+            <hr>
+            <hr>
+            <br/>
+         </div>
+         
+         <div class="page-intro">
+            <table style="width:80%">
+               <tr>
+                  <th>Html Link</th>
+                  <th>Description</th>
+               </tr>
+               <tr>
+                  <td><font color="red">index.php</td>
+                  <td>Index </td>
+               </tr>
+               <tr>
+                  <td><font color="red">Travel.php</td>
+                  <td>The Start of Assignment 2.</td>
+               </tr>         
+               
+               
+            </table>
+         </div>
+      </div>
+      
+  <?php    
+  include("Footer.php");
+  ?>
+  
+</html>
